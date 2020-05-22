e2fa19dcafe9447a7b685220ba84e2ef22c37036
dfeleppa <dfeleppa@gmail.com>
2020-05-22 09:25:56 -0400

    fix animations

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,22 +1,20 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('nav-links li');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        
-        // navLinks.forEach((link, index) => {
-        //     link.style.animation = `navLinkFade 2s ease forwards ${index/5+2}s`;
-        //     if (link.style.animation) {
-        //         link.style.animation = '';
-        //     } else {        
-        //         link.style.animation = `navLinkFade 2s ease forwards ${index/5+2}s`;
-        //     }
-        // });
         burger.classList.toggle('toggle');
-
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {        
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/5+2}s`;
+            }
+        });
     });
 }
 
 navSlide();
+
