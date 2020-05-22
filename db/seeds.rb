464324750626990581ddd6a70f018023e9daad2e
dfeleppa <dfeleppa@gmail.com>
2020-05-22 13:11:07 -0400

    repair seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Create 2 test users
 daniel = User.create(name: "Daniel", email: "daniel@daniel.com", password: "password", height: 70, weight: 205, sex: "male")
-danielle = User.create(name: "Danielle", email: "daniel@daniel.com", password: "password", height: 70, weight: 205, sex: "female")
+jen = User.create(name: "Jen", email: "daniel@daniel.com", password: "password", height: 70, weight: 205, sex: "female")
 
 # Create 2 test user posts
 UserPost.create(user_id: daniel.id, workout_name: "murph", score: 360, rx: true)
-UserPost.create(user_id: danielle.id, workout_name: "murph", score: 347, rx: true)
+UserPost.create(user_id: jen.id, workout_name: "murph", score: 347, rx: true)
